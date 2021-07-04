300c9cbc3570313fb189ea1ef60e57fa38afa47d
Sophonity <86509252+Sophonity@users.noreply.github.com>
2021-07-04 13:03:33 +0800

    update DataFormat
    DataFormat :   :+1:
    1. extend comparison operator for class task_item & period_item
    2. the self-defined list class now inherit the built-in list

--- DataFormat.py
+++ DataFormat.py
@@ -1,5 +1,6 @@
+from typing import List
 
-class task_item():
+class task_item:
     def __init__(self, name, duration, importance, deadline_date, deadline_time, min_length = 1,
             type = "None", non_consecutive_type = []) :
         self.name = name # str
@@ -11,7 +12,7 @@ class task_item():
         self.min_length = min_length # int, every 30 min or 10 min 
         self.non_consecutive_type = non_consecutive_type # list of types that can't take place right after this task
         #self.partition_num = [1,duration//min_length]
-        self.partition = [duration]
+        #self.partition = [duration]
         #for i in range():
         #    self.partition.append(min_length)
 
@@ -20,6 +21,36 @@ class task_item():
             self.min_length, self.type, self.non_consecutive_type)
         return copy
 
+    def __gt__(self,other):
+        if (self.deadline_date, self.deadline_time, self.importance) > (other.deadline_date, other.deadline_time, other.importance):
+            return True
+        else:
+            return False
+
+    def __eq__(self, other: object) -> bool:
+        if (self.deadline_date, self.deadline_time, self.importance) == (other.deadline_date, other.deadline_time, other.importance):
+            return True
+        else:
+            return False
+
+    def __lt__(self,other):
+        if (self.deadline_date, self.deadline_time, self.importance) < (other.deadline_date, other.deadline_time, other.importance):
+            return True
+        else:
+            return False
+
+    def __ge__(self,other):
+        if (self.deadline_date, self.deadline_time, self.importance) >= (other.deadline_date, other.deadline_time, other.importance):
+            return True
+        else:
+            return False
+
+    def __le__(self,other):
+        if (self.deadline_date, self.deadline_time, self.importance) <= (other.deadline_date, other.deadline_time, other.importance):
+            return True
+        else:
+            return False
+    
 class period_item:
     def __init__(self,date,begin_time,end_time):
         self.date = date
@@ -30,7 +61,38 @@ class period_item:
         copy = period_item(self.date, self.begin, self.end)
         return copy
 
-class list(): ## 可以廢掉
+    def __gt__(self,other):
+        if (self.date, self.begin, self.end) > (other.date, other.begin, other.end):
+            return True
+        else:
+            return False
+
+    def __eq__(self, other: object) -> bool:
+        if (self.date, self.begin, self.end) == (other.date, other.begin, other.end):
+            return True
+        else:
+            return False
+
+    def __lt__(self,other):
+        if (self.date, self.begin, self.end) < (other.date, other.begin, other.end):
+            return True
+        else:
+            return False
+
+    def __ge__(self,other):
+        if (self.date, self.begin, self.end) >= (other.date, other.begin, other.end):
+            return True
+        else:
+            return False
+
+    def __le__(self,other):
+        if (self.date, self.begin, self.end) <= (other.date, other.begin, other.end):
+            return True
+        else:
+            return False
+
+'''
+class list: ## 可以廢掉
     def __init__(self, list_of_tasks):
         self.list = list_of_tasks
     
@@ -48,9 +110,6 @@ class list(): ## 可以廢掉
             self.list = sorted(self.list,key= lambda period_item:(period_item.date, period_item.begin), reverse = not (rev))
         return self.list
     
-    def delete(self,task):
-        self.list.remove(task)
-
     def pop(self,index):
         self.list.pop(index)
 
@@ -76,6 +135,53 @@ class list(): ## 可以廢掉
     
     def get(self,index):
         return self.list[index]
+'''
+
+class list(List): ## 可以廢掉
+    def __init__(self, list_item):
+        super().__init__(list_item)
+    '''     
+    def add(self, task_item):
+        self.list.append(task_item)
+    
+    def delete(self, task_item):
+        self.list.remove(task_item)
+
+    def sort(self, type = "deadline", rev = True):
+        if type == "deadline":
+            self.list = sorted(self.list,key= lambda task_item:(task_item.deadline_date, task_item.deadline_time,
+                task_item.importance), reverse = not (rev))
+        elif type == "time":
+            self.list = sorted(self.list,key= lambda period_item:(period_item.date, period_item.begin), reverse = not (rev))
+        return self.list
+    
+    def pop(self,index):
+        self.list.pop(index)
+
+    def len(self):
+        return len(self.list)
+
+    def index(self,task_item):
+        return self.list.index(task_item)
+    
+    def get(self,index):
+        return self.list[index]
+    '''
+    def RemoveMin(self):
+        self.list.pop(0)
+
+    def Swap(self):
+        if len(self.list) > 1:
+            temp = self.list[0]
+            self.list[0] = self.list[1] 
+            self.list[1] = temp
+            return True
+        return False
+    
+    def Peek(self):
+        return self.list[0]
+
+import json
 
 if __name__ == "__main__": #testing
     
@@ -84,7 +190,9 @@ if __name__ == "__main__": #testing
 
     t1 = task_item("B",3,1,2,24,1,"exercise",["academy"])
     t4 = task_item("A",3,1,1,24,1,"academy",[])
-    t3 = task_item("C",3,2,3,24,1,"academy",[])
-    t2 = task_item("D",1,1,4,24,1,"exercise",[])
-    T = list([t1,t2,t3,t4])
-    
+    #t3 = task_item("C",3,2,3,24,1,"academy",[])
+    #t2 = task_item("D",1,1,4,24,1,"exercise",[])
+    #T = list([t1,t2,t3,t4])
+    print(t1,t4)
+    T = list([t1,t4])
+    print(sorted(T))
